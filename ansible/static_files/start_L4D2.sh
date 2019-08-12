2a86f41783d87c1cfbeea9a92b0a16b79db0c1ff
Marcin <marcin@vertiops.com>
2019-08-12 21:28:20 +0300

    Fixes

--- ansible/static_files/start_L4D2.sh
+++ ansible/static_files/start_L4D2.sh
@@ -1,1 +1,1 @@
-screen -d -m ./srcds_run -console -game left4dead2 +port 27020 +maxplayers 8 +exec server.cfg +map c2m1_highway
+screen -d -m ./srcds_run -console -game left4dead2 -port 27020 +exec server.cfg +map c2m1_highway
